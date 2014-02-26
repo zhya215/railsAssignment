30a398124355988440cfeaffca80c1c13974faa1
saasbook <saasbook@saasbook.(none)>
2014-02-25 21:49:36 -0400

    updated rails assignment

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,8 +7,52 @@ class MoviesController < ApplicationController
   end
 
   def index
+    @all_ratings= Movie.all_ratings
+    @ratings={}
     @movies = Movie.all
+    keys=[]
+    redirect= false
+    # This is for checking whether there is params[:ratings] or session[:ratings] exists.
+    if params[:ratings]
+      @ratings=params[:ratings]
+    else
+      if session[:ratings]
+        @ratings=session[:ratings]
+        redirect=true
+      end
+    end
+
+    if params[:sort] # Check whether params[:sort] has been set.
+      @sort=params[:sort]
+    else
+      if session[:sort]
+        @sort=session[:sort]
+        redirect=true
+      end
+    end
+
+    if redirect
+      redirect_to movies_path(:ratings => @ratings, :sort => @sort)
+    end
+
+    # If ratings is not empty.
+    if !@ratings.empty?
+      keys=@ratings.keys
+      @movies=Movie.filter(keys)
+      session[:ratings]=@ratings
+    end
+    
+    if @sort # Check whether params[:sort] has been set.
+      if !@ratings.empty?
+        @movies = Movie.sort_movies_with_filter(keys, @sort) # get sorted and filtered movies.
+      else
+        @movies= Movie.sort_movies(@sort)# get sorted movies.
+      end
+      session[:sort]=@sort
+    end
+
   end
+
 
   def new
     # default: render 'new' template
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,14 @@
 class Movie < ActiveRecord::Base
+	def self.all_ratings
+		ratings = Movie.find(:all, :select => 'rating', :group => 'rating').map(&:rating)
+	end
+	def self.sort_movies(params)
+		Movie.find(:all, :order => params)
+	end
+	def self.filter(ratings)
+		Movie.find(:all, :conditions=>{:rating => ratings})
+	end
+	def self.sort_movies_with_filter(ratings, params)
+		Movie.find(:all, :order => params, :conditions=>{:rating => ratings})
+	end
 end
